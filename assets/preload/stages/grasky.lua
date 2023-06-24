3ac66f8f40e91734fb6c8206b81dcd094df49e45
MAUROPORT <136853307+MAUROPORT@users.noreply.github.com>
2023-06-24 17:41:07 -0500

    Add files via upload

---
+++ assets/preload/stages/grasky.lua
@@ -0,0 +1,9 @@
+function onCreate()
+	makeLuaSprite('grasky', 'grasky', -422, -60);
+        scaleLuaSprite('grasky', 1.7, 1.7);
+	setLuaSpriteScrollFactor('grasky', 1.0, 1.0);
+        addLuaSprite('grasky', false);
+
+
+	close(true); 
+end
